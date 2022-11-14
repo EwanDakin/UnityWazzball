e615ce472074cbe7e7f05c955e4a5a4bc210cce9
EwanDakin <ewandakin@gmail.com>
2022-11-14 16:38:47 +0000

    UI + Expanded Map

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,13 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookAtPlayer : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
+
+    public static Vector3 Destination = new Vector3 ((float) 0.0, (float) 0.5, (float) -2.5);
+    public static Vector3 Start = new Vector3((float)0.0, (float)0.5, (float)-12.5);
+    public float enemyspeed = 2;
     public bool look = false;
     public GameObject player;
 
     void Update()
     {
+        if (Destination != Start)
+        {
+            Vector3.MoveTowards(Start,Destination,enemyspeed);
+        }
         if (look == true)
         {
             Vector3 plocation = player.transform.position;
@@ -18,3 +26,4 @@ public class LookAtPlayer : MonoBehaviour
         }
     }
 }
+
--- Assets/Scripts/TrigScript.cs
+++ Assets/Scripts/TrigScript.cs
@@ -2,27 +2,23 @@ using UnityEngine;
 
 public class TrigScript : MonoBehaviour
 {
+    public GameObject uipanel;
+    public GameObject player;
     void Start()
     {
-        
+        uipanel.SetActive(false);
     }
     void Update()
     {
-        
+ 
     }
 
     public GameObject enemy;
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Enter");
-
-        newlook();
-
-        void newlook()
-        {
-            //GameObject.AddComponent<LookAtPlayer>();
-            enemy.GetComponent<LookAtPlayer>().look = true;
-        }
-
+        enemy.GetComponent<EnemyMovement>().look = true;
+        uipanel.SetActive(true);
+        player.GetComponent<movement>().speed = 0;
     }
 }
